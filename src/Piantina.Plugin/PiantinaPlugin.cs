48a0d8108983b5f2d6b58ff8b1e12829f47b5ba6
Frootloop <christian.oldewage07@gmail.com>
2026-07-20 13:16:43 +0200

    Add project files.

---
+++ src/Piantina.Plugin/PiantinaPlugin.cs
@@ -0,0 +1,13 @@
+﻿using Rhino.PlugIns;
+
+namespace Piantina.Plugin;
+
+public class PiantinaPlugin : PlugIn
+{
+    public static PiantinaPlugin Instance { get; private set; } = null!;
+
+    public PiantinaPlugin()
+    {
+        Instance = this;
+    }
+}
